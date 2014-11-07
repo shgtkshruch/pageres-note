ac9d4ce2726bdbb4665e1a59f7a184e1e2658b08
shgtkshruch <shgtk.shruch@gmail.com>
2014-11-07 16:59:13 +0900

    get items stream in pageres collback.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = 'production';
 
+var crypto = require('crypto');
 var Pageres = require('pageres');
 var nodeNote = require('node-note');
 var cheerio = require('cheerio');
@@ -11,25 +12,44 @@ function getUrl (callback) {
   evernote.getNoteMetadata({word: 'web_design_evernote'}, function (noteMetadataList) {
     var guid = noteMetadataList[0].guid;
     evernote.getNote({guid: guid, withContent: true}, function (note) {
+      var _note = {};
       var $ = cheerio.load(note.content);
       var url = $('a').attr('shape', 'rect').first().text();
       console.log('URL:', url);
-      callback(url);
+
+      _note.title = note.title
+      _note.url = url;
+
+      callback(_note);
     });
   });
 }
 
-function getPage (url) {
+function getPage (note, callback) {
   var pageres = new Pageres({delay: 2})
-    .src(url, ['iphone 5s', '1024x768', '1920x1120'])
-    .dest(__dirname + '/images');
+    .src(note.url, ['iphone 5'], {crop: true})
 
-  pageres.run(function (err) {
+  pageres.run(function (err, items) {
     if (err) throw err;
-    console.log(url + ', done.');
+    callback(note, items);
   });
 }
 
-getUrl(function (url) {
-  getPage(url);
+function createNote (note, items) {
+  var stream = items[0];
+  var filename = stream.filename;
+  var md5 = crypto.createHash('md5');
+  var hex;
+  md5.setEncoding('hex');
+
+  stream.on('end', function () {
+    md5.end();
+    hex = md5.read();
+  });
+}
+
+getUrl(function (note) {
+  getPage(note, function (note, items) {
+    createNote(note, items);
+  });
 });
