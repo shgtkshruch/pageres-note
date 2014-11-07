f54f05c60452e0e004cc9b9646eb005d01d967b1
shgtkshruch <shgtk.shruch@gmail.com>
2014-11-08 07:52:48 +0900

    Create note with resouce.

--- index.js
+++ index.js
@@ -1,55 +1,73 @@
 process.env.NODE_ENV = 'production';
 
-var crypto = require('crypto');
+var fs = require('fs');
 var Pageres = require('pageres');
 var nodeNote = require('node-note');
 var cheerio = require('cheerio');
+var rimraf = require('rimraf');
 var config = require('./config.json');
 
 var evernote = new nodeNote(config);
+
+var sizes = ['320x480', '768x1024', '1366x768', '1920x1080'];
+var tags = ['mobile', 'tablet', 'laptop', 'desktop'];
+var dir = __dirname + '/images';
 
 function getUrl (callback) {
   evernote.getNoteMetadata({word: 'web_design_evernote'}, function (noteMetadataList) {
     var guid = noteMetadataList[0].guid;
     evernote.getNote({guid: guid, withContent: true}, function (note) {
-      var _note = {};
       var $ = cheerio.load(note.content);
       var url = $('a').attr('shape', 'rect').first().text();
-      console.log('URL:', url);
-
-      _note.title = note.title
-      _note.url = url;
-
-      callback(_note);
+      var title = $('b').first().text();
+      callback(url, title);
     });
   });
 }
 
-function getPage (note, callback) {
+function getPage (url, callback) {
   var pageres = new Pageres({delay: 2})
-    .src(note.url, ['iphone 5'], {crop: true})
+    .src(url, sizes)
+    .dest(dir);
 
-  pageres.run(function (err, items) {
+  pageres.run(function (err) {
     if (err) throw err;
-    callback(note, items);
+    callback();
   });
 }
 
-function createNote (note, items) {
-  var stream = items[0];
-  var filename = stream.filename;
-  var md5 = crypto.createHash('md5');
-  var hex;
-  md5.setEncoding('hex');
-
-  stream.on('end', function () {
-    md5.end();
-    hex = md5.read();
+function createNote (_title, file, url, tag) {
+  var title = _title + ' (' + tag + ')';
+  var options = {
+    title: title,
+    author: 'shgtkshruch',
+    file: file,
+    url: url,
+    tag: [tag],
+    notebookName: '1511 Responsive'
+  }
+  evernote.createNote(options, function (note) {
+    console.log('Create', title, 'note.');
   });
 }
 
-getUrl(function (note) {
-  getPage(note, function (note, items) {
-    createNote(note, items);
+getUrl(function (url, title) {
+  getPage(url, function () {
+    fs.readdir(dir, function (err, files) {
+      var files = files.sort(function (a, b) {
+        var re = /\-(\d+)x.+\.png$/;
+        var a = a.match(re);
+        var b = b.match(re);
+        return a[1] - b[1];
+      });
+      files.forEach(function (file, i) {
+        var file = dir + '/' + file;
+        createNote(title, file, url, tags[i]);
+      });
+      // rimraf(dir, function () {
+      //   console.log('delete', dir);
+      // });
+    });
   });
 });
+
