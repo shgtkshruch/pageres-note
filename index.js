b3373dba69f9e8d83af8c6701132d9d2178f8ad0
shgtkshruch <shgtk.shruch@gmail.com>
2014-11-09 10:23:56 +0900

    Upload new note to Evernote.

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ process.env.NODE_ENV = 'production';
 var fs = require('fs');
 var Pageres = require('pageres');
 var nodeNote = require('node-note');
+var _ = require('lodash');
+var async = require('async');
 var cheerio = require('cheerio');
 var rimraf = require('rimraf');
 var config = require('./config.json');
 
 var evernote = new nodeNote(config);
 
-var sizes = ['320x480', '768x1024', '1366x768', '1920x1080'];
-var tags = ['mobile', 'tablet', 'laptop', 'desktop'];
+var sizes = {
+  'mobile': '320x480',
+  'tablet': '768x1024',
+  'laptop': '1366x768',
+  'desktop': '1920x1080'
+};
+
 var dir = __dirname + '/images';
 
 function getUrl (callback) {
@@ -26,48 +33,69 @@ function getUrl (callback) {
 }
 
 function getPage (url, callback) {
-  var pageres = new Pageres({delay: 2})
-    .src(url, sizes)
+  var pageres = new Pageres({delay: 2, crop: true})
+    .src(url, _.values(sizes))
     .dest(dir);
 
   pageres.run(function (err) {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    }
     callback();
   });
 }
 
-function createNote (_title, file, url, tag) {
+function createNote (_title, fp, url, tag, width, done) {
   var title = _title + ' (' + tag + ')';
   var options = {
     title: title,
     author: 'shgtkshruch',
-    file: file,
+    file: fp,
     url: url,
     tag: [tag],
+    width: width + 'px',
     notebookName: '1511 Responsive'
-  }
+  };
   evernote.createNote(options, function (note) {
     console.log('Create', title, 'note.');
+    done();
+  });
+}
+
+function file (url, title) {
+  fs.readdir(dir, function (err, _files) {
+    var reFile = /(\d+)x\d+(?:\-cropped)?\.png$/;
+    var files = _files.sort(function (a, b) {
+      var sizeA= a.match(reFile)[1];
+      var sizeB = b.match(reFile)[1];
+      return sizeA - sizeB;
+    });
+    async.eachSeries(files, function (file, done) {
+      var width = file.match(reFile)[1];
+      var fp = dir + '/' + file;
+      var tag = _.findKey(sizes, function (size) {
+        var re = new RegExp(width);
+        return size.match(re);
+      });
+
+      createNote(title, fp, url, tag, width, done);
+    }, function (err) {
+      if (err) {
+        throw err;
+      }
+      rimraf(dir, function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log('Delete', dir);
+      });
+    });
   });
 }
 
 getUrl(function (url, title) {
   getPage(url, function () {
-    fs.readdir(dir, function (err, files) {
-      var files = files.sort(function (a, b) {
-        var re = /\-(\d+)x.+\.png$/;
-        var a = a.match(re);
-        var b = b.match(re);
-        return a[1] - b[1];
-      });
-      files.forEach(function (file, i) {
-        var file = dir + '/' + file;
-        createNote(title, file, url, tags[i]);
-      });
-      // rimraf(dir, function () {
-      //   console.log('delete', dir);
-      // });
-    });
+    file(url, title);
   });
 });
 
