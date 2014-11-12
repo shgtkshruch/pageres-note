7313d02c22f46a65544203d8436f60b32cd2a92f
shgtkshruch <shgtk.shruch@gmail.com>
2014-11-12 16:38:21 +0900

    Expose main loop.

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ var config = require('./config.json');
 var evernote = new nodeNote(config);
 var dir = __dirname + '/images';
 
-// configuration
 var sizes = {
   'mobile': '320x480',
   'tablet': '768x1024',
@@ -22,42 +21,43 @@ var sizes = {
 };
 
 var keyword = 'web_design_evernote';
+var num = 20;
+
+evernote.getNoteMetadata({word: keyword, maxNotes: num}, function (datas) {
+  if (datas.length === 0) {
+    console.log(keyword, 'note not found.');
+    return;
+  }
+  console.log("Notes number:", datas.length);
+  each(datas, function (data, nextNote) {
+    evernote.getNote({guid: data.guid, withContent: true}, function (note) {
+      create(note, nextNote);
+    });
+  }, function (err) {
+    if (err) throw err;
+    console.log('All task done.');
+  });
+});
 
 /**
- * Get note ooject from Evernote
+ * Crate new note object
  *
- * @param {String} search evernote by this
- * @param {Number} max number of return note
- * @param {Function} callback
+ * @param {Object} fetched note from evernote
  */
 
-function fetch (keyword, maxNotes, cb) {
-  var notes = [];
-  evernote.getNoteMetadata({word: keyword, maxNotes: maxNotes}, function (datas) {
-    each(datas, function (data, done) {
-      evernote.getNote({guid: data.guid, withContent: true}, function (note) {
-        create(notes, note, done);
-      });
-    }, function (err) {
-      if (err) throw err;
-      cb(notes);
-    });
-  });
-}
+function create (note, nextNote) {
+  getTitle(note, function (title) {
+    console.log('Start:', title);
+    var n = {};
+    var $ = cheerio.load(note.content);
+    var tag = note.content.match(/tag\s((\w+\s?){1,})</);
 
-function create (notes, note, next) {
-  var n = {};
-  var $ = cheerio.load(note.content);
-  var tag = note.content.match(/tag\s((\w+\s?){1,})</);
-
-  n.url = $('a').first().text();
-  n.guid = note.guid;
-  n.title = note.title.replace(/\[feedly\]/, '').trim();
-  n.tag = tag ? tag[1].split(' ') : [];
-  getTitle(n, function (title) {
     n.title = title;
-    notes.push(n);
-    next();
+    n.url = $('a').first().text();
+    n.guid = note.guid;
+    n.title = note.title.replace(/\[feedly\]/, '').trim();
+    n.tag = tag ? tag[1].split(' ') : [];
+    get(n, nextNote);
   });
 }
 
@@ -69,15 +69,15 @@ function create (notes, note, next) {
  */
 
 function getTitle (note, cb) {
-  if (note.title !== 'Mailed in note') {
-    cb(note.title);
-  } else {
+  if (note.title === 'Mailed in note' || note.title === '無題ノート') {
     request(note.url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var $ = cheerio.load(body);
         cb($('title').text());
       }
     });
+  } else {
+    cb(note.title);
   }
 }
 
@@ -89,8 +89,8 @@ function getTitle (note, cb) {
  * @param {Function} callback
  */
 
-function get (note, sizes, next) {
-  new Pageres({delay: 2})
+function get (note, nextNote) {
+  new Pageres({delay: 3})
     .src(note.url, _.values(sizes))
     .dest(dir)
     .run(function (err) {
@@ -100,7 +100,7 @@ function get (note, sizes, next) {
       var files = fs.readdirSync(dir).sort(function (a, b) {
         return a.match(width)[1] - b.match(width)[1];
       });
-      file(files, note, next);
+      file(files, note, nextNote);
     });
 }
 
@@ -111,7 +111,7 @@ function get (note, sizes, next) {
  * @param {Function} callback
  */
 
-function file (files, note, next) {
+function file (files, note, nextNote) {
   var i = 0;
   each(files, function (file, nextFile) {
     var n = _.clone(note, true);
@@ -121,7 +121,7 @@ function file (files, note, next) {
     save(n, nextFile);
   }, function (err) {
     if (err) throw err;
-    remove(note, next);
+    remove(note, nextNote);
   });
 }
 
@@ -132,7 +132,7 @@ function file (files, note, next) {
  * @param {Function} callback
  */
 
-function save (note, done) {
+function save (note, nextFile) {
   var options = {
     title: note.title + ' (' + _.last(note.tag) + ')',
     file: note.fp,
@@ -144,7 +144,7 @@ function save (note, done) {
   };
   evernote.createNote(options, function (note) {
     console.log('Create', options.title, 'note.');
-    done();
+    nextFile();
   });
 }
 
@@ -155,23 +155,12 @@ function save (note, done) {
  * @param {callback} async callback
  */
 
-function remove (note, done) {
+function remove (note, nextNote) {
   rimraf(dir, function (err) {
     if (err) throw err;
     evernote.deleteNote({guid: note.guid}, function (note) {
       console.log('Delete note:', note.title);
-      done();
+      nextNote();
     });
   });
 }
-
-fetch(keyword, 50, function (notes) {
-  console.log("Notes number:", notes.length);
-  each(notes, function (note, next) {
-    console.log('Start:', note.title);
-    get(note, sizes, next);
-  }, function (err) {
-    if (err) throw err;
-    console.log('All task done.');
-  });
-});
